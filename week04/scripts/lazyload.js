3eafaca600d3d6c4eece46744d512ff04a4e8a98
andre-barrera <andre.barrera.99@gmail.com>
2025-07-22 22:06:37 -0600

    updated changes

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,27 @@
+const year = document.querySelector('#currentyear');
+const lastModification = document.querySelector('#lastModification');
+
+const today = new Date();
+
+year.innerHTML = `&copy ${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll("main img");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+        observer.unobserve(entry.target); // optional: stop watching once faded in
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  images.forEach(img => {
+    observer.observe(img);
+  });
+
+})
